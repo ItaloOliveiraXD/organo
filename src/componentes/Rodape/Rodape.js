2aa86456131e8f47877de7411f8a44c99561d5e7
Italo.Oliveira <italooliveira-xd@hotmail.com>
2023-07-26 09:16:10 -0300

    Rodapé

--- src/componentes/Rodape/Rodape.js
+++ src/componentes/Rodape/Rodape.js
@@ -1,9 +1,17 @@
 import './Rodape.css'
 
 export const Rodape = () => {
-    return(
+    return (
         <footer className='rodape'>
-            <img className="fundo" src="./images/add.png" alt="" />
+            <div className='rodape-itens'>
+                <div className='social-midia'>
+                    <img src="./images/fb.png" alt="" />
+                    <img src="./images/ig.png" alt="" />
+                    <img src="./images/tw.png" alt="" />
+                </div>
+                <img src="./images/logo.png" alt="" />
+                <h6>Desenvolvido por Italo.</h6>
+            </div>
         </footer>
     )
 }
